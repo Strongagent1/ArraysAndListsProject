e449af41f6dad9c04440a271e7e3c1ad768109e9
kyle jenne <strongagent1@gmail.com>
2022-10-26 12:48:43 -0400

    ArraysLists Project KJ

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace ArraysAndLists
 {
@@ -7,14 +9,34 @@ namespace ArraysAndLists
         static void Main(string[] args)
         {
             //TODO:
+            var nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             // Create an int Array and populate numbers 1-10
-
+            var evens = new List<int>();
+            var odds = new List<int>();
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
-
+            foreach(int i in nums)
+            {
+                if (i % 2 == 0)
+                {
+                    evens.Add(i);
+                }
+                else
+                {
+                    odds.Add(i);
+                }
+            }
+            foreach (int i in evens)
+            {
+                Console.WriteLine($"{i} is even-steven.");
+            }
+            foreach (int i in odds)
+            {
+                Console.WriteLine($"{i} is an odd looking number.");
+            }
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
              *  if a number is even or odd.
